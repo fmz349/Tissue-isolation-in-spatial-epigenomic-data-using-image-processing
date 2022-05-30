688b3f3fa30c5a259a1589d69f877f1e27f313df
Wenqi Wang <70809681+fmz349@users.noreply.github.com>
2022-05-30 21:52:53 +0200

    Add files via upload

---
+++ ME11_H3K27ac_50um/new_vesalius_h3k27ac.R
@@ -0,0 +1,190 @@
+library(Matrix)
+library(parallel)
+library(imager)
+library(imagerExtra)
+library(Seurat)
+library(tidyverse)
+library(patchwork)
+library(viridis)
+library(RColorBrewer)
+library(tvR)
+library(sp)
+library(grid)
+library(gridExtra)
+library(ggplot2)
+
+library(SuperpixelImageSegmentation)
+
+library(Signac)
+library(tidyverse)
+
+# Standard libraries - just in case you are a command line fiend
+library(utils)
+library(stats)
+library(graphics)
+library(grDevices)
+
+# load new version vesalius
+setwd("~/spatial_cuttag_project/Vesalius/R")
+files.sources = list.files()
+files.sources = files.sources[-1]
+sapply(files.sources, source)
+
+# firstly build vesalius object from seurat object
+# read in seurat object
+object <- readRDS("~/ME11_H3K27ac_50um/seurat_object")
+beads <- GetTissueCoordinates(object@images$slice1)
+beads$barcodes <- rownames(beads)
+colnames(beads) <- c("xcoord","ycoord","barcodes")
+
+counts <- GetAssayData(object, slot = "counts")
+
+vesalius <- buildVesaliusObject(beads,counts)
+
+# building vesalius embeddings
+ves <- buildVesaliusEmbeddings(vesalius,
+                               norm = "TFIDF",
+                               method ="LSI",
+                               pcs = 30,
+                               tensorResolution = 0.1,
+                               cores = 5)
+
+saveRDS(ves,file="~/ME11_H3K27ac_50um/new_vesalius/new_vesalius_embedding.Rds")
+
+# make the grayscale plots for principal components
+
+p <- list()
+
+for(i in 1:30){
+  p[[i]] <- imagePlot(ves, dims = i, cex = 12)+theme_void()+coord_flip()+scale_x_reverse()
+}
+do.call(grid.arrange,p)
+
+
+# equalize histogram
+ves_eh <- equalizeHistogram(
+  ves,
+  embedding = "LSI",
+  dims = 1:30,
+  sleft = 15,
+  sright = 15
+)
+
+p3 <- list()
+
+for(i in 1:30){
+  p3[[i]] <- imagePlot(ves_eh, dims = i, cex = 12)+theme_void()+coord_flip()+scale_x_reverse()
+}
+do.call(grid.arrange,p3)
+
+# image segmentation
+seg <-
+  imageSegmentation(
+    ves_eh,
+    dims = 1:30,
+    colDepth = 10,
+    smoothIter = 5,
+    smoothType = "iso",
+    sigma = 1,
+    embedding = "LSI"
+  )
+
+territoryPlot(seg,cex.pt = 2)+coord_flip()+scale_x_reverse()+theme(
+  plot.title = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  axis.line=element_blank(),
+  axis.text.x=element_blank(),
+  axis.text.y=element_blank(),
+  axis.ticks=element_blank())
+
+# territory isolation
+ter <- isolateTerritories(seg,trial = "last")
+
+territoryPlot(ter,cex.pt = 2)+coord_flip()+scale_x_reverse()+theme(
+  plot.title = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  axis.line=element_blank(),
+  axis.text.x=element_blank(),
+  axis.text.y=element_blank(),
+  axis.ticks=element_blank())
+
+territoryPlot(ter,cex.pt = 0.1,split = TRUE)+coord_flip()+scale_x_reverse()+theme(
+  plot.title = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  axis.line=element_blank(),
+  axis.text.x=element_blank(),
+  axis.text.y=element_blank(),
+  axis.ticks=element_blank())
+
+saveRDS(ter,"~/ME11_H3K27ac_50um/new_vesalius/ter")
+
+territoryPlot <- function(vesalius,
+                          trial = "last",
+                          split = FALSE,
+                          randomise = TRUE,
+                          cex=10,
+                          cex.pt=0.25){
+  
+  if(!is.null(vesalius@territories) & trial == "last"){
+    trial <- colnames(vesalius@territories)[ncol(vesalius@territories)]
+    ter <- vesalius@territories[,c("x","y",trial)]
+    colnames(ter) <- c("x","y","territory")
+    ter$territory <- as.factor(ter$territory)
+    legend <- sapply(strsplit(trial,"_"),"[[",1)
+  } else if(!is.null(vesalius@territories) & trial != "last") {
+    if(length(grep(x = colnames(vesalius@territories),pattern = trial))==0){
+      stop(paste(deparse(substitute(trial)),"is not in territory data frame"))
+    }
+    ter <- vesalius@territories[,c("x","y",trial)]
+    colnames(ter) <- c("x","y","territory")
+    ter$territory <- as.factor(ter$territory)
+    legend <- sapply(strsplit(trial,"_"),"[[",1)
+    
+  } else {
+    stop("No territories have been computed!")
+  }
+  
+  ter_col <- length(levels(ter$territory))
+  ter_pal <- colorRampPalette(brewer.pal(8, "Accent"))
+  
+  if(randomise){
+    ter_col <- sample(ter_pal(ter_col),ter_col)
+  } else {
+    ter_col <- ter_pal(ter_col)
+  }
+  if(split){
+    terPlot <- ggplot(ter, aes(x,y,col = territory)) +
+      geom_point(size= cex.pt, alpha = 0.65)+
+      facet_wrap(~territory)+
+      theme_classic() +
+      scale_color_manual(values = ter_col)+
+      theme(legend.text = element_text(size = cex *1.2),
+            axis.text = element_text(size = cex *1.2),
+            axis.title = element_text(size = cex *1.2),
+            plot.tag = element_text(size=cex * 2),
+            plot.title = element_text(size=cex * 1.5),
+            legend.title = element_text(size=cex * 1.2)) +
+      guides(colour = guide_legend(override.aes = list(size=cex * 0.3)))+
+      labs(colour = legend, title = paste("Vesalius",trial),
+           x = "X coordinates", y = "Y coordinates")
+  } else {
+    terPlot <- ggplot(ter, aes(x,y,col = territory)) +
+      geom_point(size= cex.pt, alpha = 0.65)+
+      theme_classic() +
+      scale_color_manual(values = ter_col)+
+      theme(legend.text = element_text(size = cex *1.2),
+            axis.text = element_text(size = cex *1.2),
+            axis.title = element_text(size = cex *1.2),
+            plot.tag = element_text(size=cex * 2),
+            plot.title = element_text(size=cex * 1.5),
+            legend.title = element_text(size=cex * 1.2)) +
+      guides(colour = guide_legend(override.aes = list(size=cex * 0.3)))+
+      labs(colour = legend, title = paste("Vesalius",trial),
+           x = "X coordinates", y = "Y coordinates")
+  }
+  
+  return(terPlot)
+}
---
+++ ME11_H3K27ac_50um/transfer_MOCA.R
@@ -0,0 +1,133 @@
+library(ArchR)
+library(Seurat)
+library(grid)
+library(Signac)
+library(patchwork)
+library(tidyverse)
+library(mclust)
+
+addArchRThreads(threads = 5)
+addArchRGenome("mm10")
+
+MOCA_E11 <- readRDS("~/spatial_cuttag_project/ref_data/MOCA_E11_seurat_object")
+
+# read in the ArchRProj in tissue
+proj_in_tissue <- loadArchRProject("~/ME11_H3K27ac_50um/Save-Proj_in_tissue1")
+
+proj_in_tissue <- addIterativeLSI(
+  ArchRProj = proj_in_tissue,
+  useMatrix = "TileMatrix", 
+  name = "IterativeLSI", 
+  iterations = 2, 
+  clusterParams = list(
+    resolution = c(0.2), 
+    sampleCells = 10000, 
+    n.start = 10
+  ), 
+  varFeatures = 25000, 
+  dimsToUse = 1:30,
+  force = TRUE
+)
+
+print("LSI has finished")
+
+proj_in_tissue <- addImputeWeights(proj_in_tissue)
+
+# estimate cell identity
+proj_in_tissue <- addGeneIntegrationMatrix(
+  ArchRProj = proj_in_tissue, 
+  useMatrix = "GeneScoreMatrix",
+  matrixName = "GeneIntegrationMatrix",
+  reducedDims = "IterativeLSI",
+  seRNA = MOCA_E11,
+  addToArrow = TRUE,
+  groupRNA = "Main_cell_type",
+  nameCell = "predictedCell",
+  nameGroup = "predictedGroup",
+  nameScore = "predictedScore",
+  force = TRUE
+)
+
+saveArchRProject(ArchRProj = proj_in_tissue,
+                 outputDirectory = "~/ME11_H3K27ac_50um/ArchR_Proj_in_tissue_predicted",
+                 load = FALSE)
+
+
+print("prediction finished")
+
+meta.data.integration <- as.data.frame(getCellColData(ArchRProj = proj_in_tissue))[, c('predictedCell', 'predictedGroup', 'predictedScore')]
+new_row_names <- row.names(meta.data.integration)
+new_row_names <- unlist(lapply(new_row_names, function(x) gsub(".*#","", x)))
+new_row_names <- unlist(lapply(new_row_names, function(x) gsub("-.*","", x)))
+row.names(meta.data.integration) <- new_row_names
+
+# add prediction to seurat object as metadata
+spatial.obj <- readRDS("~/ME11_H3K27ac_50um/seurat_object")
+spatial.obj <- AddMetaData(object = spatial.obj, metadata = meta.data.integration)
+
+saveRDS(spatial.obj, file = "~/ME11_H3K27ac_50um/seurat_object_predicted")
+
+print("integrated seraut object has been saved")
+
+# select predict group and territory
+iso_vesalius <- ter@territories
+terr.df <- iso_vesalius %>% select(barcodes, Territory_Trial_1)
+terr.df2 <- terr.df[!duplicated(terr.df$barcodes),]
+
+spatial.obj@meta.data$barcodes <- rownames(spatial.obj@meta.data)
+
+combine <- merge(terr.df2,spatial.obj@meta.data,by="barcodes")
+rownames(combine) <- combine$barcodes
+
+object2 <- subset(spatial.obj, cells = combine$barcodes)
+object2 <- AddMetaData(object = object2, metadata = combine)
+
+saveRDS(object2,"~/ME11_H3K27ac_50um/new_vesalius/seurat.predictgroup.ter")
+
+group.ter <- object2@meta.data[c("predictedGroup","Territory_Trial_1")]
+
+# plot different predict groups
+source('~/spatial_cuttag_project/downloaded_Data_visualization_code/Data_visualization/SpatialDimPlot_new.R')
+
+Idents(object2) <- 'predictedGroup'
+
+# the cell group we want to show
+ids.highlight <- "Oligodendrocyte Progenitors"
+
+p <- SpatialDimPlot_new(
+  object2,
+  cells.highlight = CellsByIdentities(object = object2, idents = ids.highlight),
+  facet.highlight = TRUE,
+  pt.size.factor = 3,
+  alpha = c(1, 0),
+  stroke = 0
+)
+p$layers[[1]]$aes_params <- c(p$layers[[1]]$aes_params, shape=22)
+p
+
+# plot different ter
+Idents(object2) <- 'Territory_Trial_1'
+ids.highlight <- "19"
+  
+p <- SpatialDimPlot_new(
+    object2,
+    cells.highlight = CellsByIdentities(object = object2, idents = ids.highlight),
+    facet.highlight = TRUE,
+    pt.size.factor = 3,
+    alpha = c(1, 0),
+    stroke = 0
+  )
+p$layers[[1]]$aes_params <- c(p$layers[[1]]$aes_params, shape=22)
+p
+
+
+
+
+
+
+
+
+
+
+
+
